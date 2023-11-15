6a85f56f6416058545535b59e362a34addf4d8a3
Saveliy Andronov <ssandry.github@gmail.com>
2023-11-15 18:31:56 +0300

    Add module

---
+++ modules/base64-to-dataurl-generator/src/index.ts
@@ -0,0 +1,9 @@
+class Base64ToDataURLGenerator {
+  /**
+   * Generate dataURL
+   * @requirement UF/FINAL-IMAGE/DATA-URL
+   */
+  public async generateDataURL(): Promise<void> {}
+}
+
+export { Base64ToDataURLGenerator };
