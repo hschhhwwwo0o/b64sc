65a1eda2b13ecc80ea8f4203c35b0c9ff231bc69
Saveliy Andronov <ssandry.github@gmail.com>
2023-11-15 18:36:22 +0300

    Module fixs

--- modules/base64-to-dataurl-generator/src/index.ts
+++ modules/base64-to-dataurl-generator/src/index.ts
@@ -3,7 +3,10 @@ class Base64ToDataURLGenerator {
    * Generate dataURL
    * @requirement UF/FINAL-IMAGE/DATA-URL
    */
-  public async generateDataURL(): Promise<void> {}
+  public async generateDataURL(base64 = ""): Promise<string> {
+    const dataUrl = `data:image/png;base64,${base64}`;
+    return dataUrl;
+  }
 }
 
 export { Base64ToDataURLGenerator };
