ecb6b2e1af4303ca29f9147b4fea4a91252e1124
Saveliy Andronov <ssandry.github@gmail.com>
2023-11-19 12:52:54 +0300

    Fix

--- app/nuxt-b64sc/app.vue
+++ app/nuxt-b64sc/app.vue
@@ -1,6 +1,7 @@
 <template>
   <div>
     <default-layout with-separated-b-g>
+      <textarea-input :style="`contrast`" />
       <text-button color="danger"> Clear </text-button>
     </default-layout>
   </div>
@@ -9,8 +10,9 @@
 <script>
 import DefaultLayout from "./components/layouts/DefaultLayout.vue";
 import TextButton from "./components/form/TextButton.vue";
+import TextareaInput from "./components/form/TextareaInput.vue";
 
 export default {
-  components: { DefaultLayout, TextButton },
+  components: { DefaultLayout, TextButton, TextareaInput },
 };
 </script>
---
+++ app/nuxt-b64sc/components/form/TextareaInput.vue
@@ -0,0 +1,88 @@
+<template>
+  <span class="container">
+    <textarea
+      :class="`${styleClass}`"
+      :name="name"
+      :placeholder="placeholder"
+    />
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    style: {
+      type: String,
+      default: "standart",
+      validator(value) {
+        ["standart", "contrast"].includes(value);
+      },
+      required: true,
+    },
+    placeholder: {
+      type: String,
+      default: "Enter...",
+      required: true,
+    },
+    name: {
+      type: String,
+      default: "textarea.",
+      required: true,
+    },
+  },
+  computed: {
+    styleClass() {
+      if (this.style === "standart") {
+        return "standart";
+      }
+      if (this.style === "contrast") {
+        return "contrast";
+      }
+      return "standart";
+    },
+  },
+};
+</script>
+
+<style lang="scss" scoped>
+@import "../../assets/styles/_vars.scss";
+
+.container {
+  display: block;
+  margin-top: 4px;
+}
+
+.contrast {
+  width: 100%;
+  max-width: 600px;
+  padding-left: 12px;
+  padding-right: 12px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: transparent;
+  border-color: $brandColor;
+  color: $brandColor;
+
+  &::placeholder {
+    color: $brandColor;
+    opacity: 0.5;
+  }
+}
+
+.standart {
+  width: 100%;
+  max-width: 600px;
+  padding-left: 12px;
+  padding-right: 12px;
+  padding-top: 10px;
+  padding-bottom: 10px;
+  background-color: transparent;
+  border-color: $white;
+  color: $white;
+
+  &::placeholder {
+    color: $white;
+    opacity: 0.5;
+  }
+}
+</style>
