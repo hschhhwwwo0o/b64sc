1e86161225c2762193966baecd14dcfd2ab25c1d
Saveliy Andronov <ssandry.github@gmail.com>
2023-11-19 12:23:11 +0300

    Fix

--- app/nuxt-b64sc/app.vue
+++ app/nuxt-b64sc/app.vue
@@ -1,13 +1,16 @@
 <template>
   <div>
-    <default-layout with-separated-b-g></default-layout>
+    <default-layout with-separated-b-g>
+      <text-button color="danger"> Clear </text-button>
+    </default-layout>
   </div>
 </template>
 
 <script>
 import DefaultLayout from "./components/layouts/DefaultLayout.vue";
+import TextButton from "./components/form/TextButton.vue";
 
 export default {
-  components: { DefaultLayout },
+  components: { DefaultLayout, TextButton },
 };
 </script>
---
+++ app/nuxt-b64sc/components/form/TextButton.vue
@@ -0,0 +1,69 @@
+<template>
+  <span class="container">
+    <button :class="`button ${textColorClass}`" @click="$emit('click')">
+      <slot />
+    </button>
+  </span>
+</template>
+
+<script>
+export default {
+  props: {
+    color: {
+      type: String,
+      default: "white",
+      validator(value) {
+        return ["white", "contrast", "danger"].includes(value);
+      },
+    },
+  },
+  emits: {
+    click: null,
+  },
+  computed: {
+    textColorClass() {
+      if (this.color === "white") {
+        return "buttonWhiteTextColor";
+      }
+      if (this.color === "contrast") {
+        return "buttonContrastTextColor";
+      }
+      if (this.color === "danger") {
+        return "buttonDangerTextColor";
+      }
+      return "buttonWhiteTextColor";
+    },
+  },
+};
+</script>
+
+<style lang="scss">
+@import "../../assets/styles/_vars.scss";
+
+.container {
+  display: block;
+}
+.button {
+  background-color: transparent;
+  border: none;
+  cursor: pointer;
+  font-family: Playfair Display;
+  font-size: 14px;
+  font-style: normal;
+  font-weight: 400;
+  line-height: normal;
+  text-decoration-line: underline;
+}
+
+.buttonWhiteTextColor {
+  color: #adadad;
+}
+
+.buttonContrastTextColor {
+  color: $brandColor;
+}
+
+.buttonDangerTextColor {
+  color: $red;
+}
+</style>
