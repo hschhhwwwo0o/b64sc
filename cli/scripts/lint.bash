c3e36a2b105029326d6867c41d900514c5547ec9
Saveliy Andronov <ssandry.github@gmail.com>
2023-12-17 10:49:34 +0300

    Fix lint cli

--- cli/scripts/lint.bash
+++ cli/scripts/lint.bash
@@ -7,6 +7,12 @@
 
 # Lint modules
 {
+  cd ./modules/base64-to-dataurl-generator && npm run lint
+  cd ../../
+
+  cd ./modules/html-image-controller && npm run lint
+  cd ../../
+
   cd ./modules/feedback-service && npm run lint
   cd ../../
 }
