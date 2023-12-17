3c16f4b640a151cb100fe7aa87236ffc7273fd89
Saveliy Andronov <ssandry.github@gmail.com>
2023-12-17 12:49:54 +0300

    Some laptop adaptive preview image

--- app/nuxt-b64sc/modules-compositions/Base64Converter/controllers/index.ts
+++ app/nuxt-b64sc/modules-compositions/Base64Converter/controllers/index.ts
@@ -1,17 +1,23 @@
+import { Base64ToDataURLGenerator } from "module/base64-to-dataurl-generator";
+
 import { defineStore } from "pinia";
 
 export const useBase64StringStore = defineStore("base64String", {
   state: () => {
     return {
       base64String: "",
+      dataUrl: "",
     };
   },
   actions: {
     /**
      * @requirement UF/INPUT-BASE64
      */
-    changeBase64String(newValue: string = ""): void {
+    async changeBase64String(newValue: string = ""): Promise<void> {
+      const base64ToDataURLGenerator = new Base64ToDataURLGenerator();
       this.base64String = newValue;
+      const dataUrl = await base64ToDataURLGenerator.generateDataURL(newValue);
+      this.dataUrl = dataUrl || "";
     },
   },
 });
--- app/nuxt-b64sc/modules-compositions/Base64Converter/view/Base64Preview.vue
+++ app/nuxt-b64sc/modules-compositions/Base64Converter/view/Base64Preview.vue
@@ -1,7 +1,7 @@
 <template>
   <!-- @requirement UF/FINAL-IMAGE/PREVIEW -->
-  <div class="preview">
-    <div class="text-cont">
+  <div :class="!dataUrl ? 'preview-text' : 'preview-image'">
+    <div v-if="!dataUrl" class="text-cont">
       <paragraph-text :color="'ultra-light'">
         &nbsp; &nbsp; &nbsp; &nbsp; This window will display the image after
         inserting the base64 string into the corresponding field.
@@ -11,6 +11,7 @@
         &nbsp; &nbsp; &nbsp; &nbsp; Now the image is not loaded.
       </paragraph-text>
     </div>
+    <img v-if="dataUrl" :src="dataUrl" alt="Image" />
   </div>
 </template>
 
@@ -23,7 +24,7 @@ import ParagraphText from "../../../components/text/ParagraphText.vue";
 export default {
   components: { ParagraphText },
   computed: {
-    ...mapWritableState(useBase64StringStore, ["base64String"]),
+    ...mapWritableState(useBase64StringStore, ["base64String", "dataUrl"]),
     ...mapStores(useBase64StringStore),
   },
 };
@@ -32,7 +33,7 @@ export default {
 <style lang="scss" scoped>
 @import "../../../assets/styles/_vars.scss";
 
-.preview {
+.preview-text {
   width: 100%;
   height: 100%;
   display: flex;
@@ -40,6 +41,27 @@ export default {
   flex-grow: 1;
   flex-wrap: wrap;
   padding: 25px;
+  box-sizing: border-box;
+  max-height: 550px;
+}
+
+.preview-image {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  background-color: $brandColor;
+  flex-grow: 1;
+  flex-wrap: wrap;
+  padding: 25px;
+  box-sizing: border-box;
+  align-items: center;
+  justify-content: center;
+  max-height: 550px;
+}
+
+.preview-image img {
+  max-width: 100%;
+  max-height: 400px;
 }
 
 .text-cont {
