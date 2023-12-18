e0fa1b6ff49d979197a5b5ee5e35eab2ef02bc7d
Saveliy Andronov <ssandry.github@gmail.com>
2023-12-18 19:58:34 +0300

    Refactoring

--- modules/html-image-controller/src/requirements/_downloadHTMLImage.ts
+++ modules/html-image-controller/src/requirements/_downloadHTMLImage.ts
@@ -2,8 +2,8 @@
  * Download image
  * @requirement UF/FINAL-IMAGE/DOWNLOAD
  */
-export function _downloadHTMLImage(src: string = "") {
-  const _linkElement = document.createElement("a");
+export function _downloadHTMLImage(src: string = ""): void {
+  const _linkElement: HTMLAnchorElement = document.createElement("a");
   _linkElement.href = src;
   _linkElement.download = `b64sc-${new Date().getTime()}`;
   document.body.appendChild(_linkElement);
